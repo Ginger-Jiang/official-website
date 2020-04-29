89d244e818ef6bdb4a856d20e0d70ee1260e873c
sanyuan <sanyuan.jw@foxmail.com>
2020-04-29 17:28:45 +0800

    添加: 添加静态资源目录配置

--- egg-server/config/config.default.js
+++ egg-server/config/config.default.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+const path = require('path');
+
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
@@ -15,6 +17,7 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_ginger';
 
+  // 模板配置
   config.view = {
     defaultViewEngine: 'nunjucks',
     mapping: {
@@ -22,14 +25,26 @@ module.exports = appInfo => {
     },
   };
 
-  // add your middleware config here
+  // 静态资源配置
+  config.static = {
+    // 静态化访问前缀,如：`http://127.0.0.1:7001/static/images/logo.png`
+    prefix: '/static',
+    dir: path.join(appInfo.baseDir, 'app/public'), // `String` or `Array:[dir1, dir2, ...]` 静态化目录,可以设置多个静态化目录
+    dynamic: true, // 如果当前访问的静态资源没有缓存，则缓存静态文件，和`preload`配合使用；
+    preload: false,
+    maxAge: 31536000, // in prod env, 0 in other envs
+    buffer: true, // in prod env, false in other envs
+  };
+
+  // 中间件配置
   config.middleware = [];
 
-  // add your user config here
+  // 用户配置
   const userConfig = {
     // myAppName: 'egg',
   };
 
+  // 数据库配置
   config.mysql = {
     client: {
       host: '62.234.182.13',
